15b296f281d5a3b999698e8b791a6df612e8ae88
Keith Mattison <klmattis@ncsu.edu>
2019-04-27 22:41:56 -0400

    updating factory.js

--- factory.js
+++ factory.js
@@ -1,8 +1,8 @@
 import web3 from '../web3';
-import VitaeFactory from './build/contracts/VitaeFactory.json';
+import VitaeFactory from './build/VitaeFactory.json';
 
 const instance = new web3.eth.Contract(
-	VitaeFactory.abi,
+	JSON.parse(VitaeFactory.interface),
 	'0xF3DE7BA6869dbEB907F6aFcD6E43F21fd44E8248'
 );
 
